a4a289173dbd0388c7c750a7c58da5d73731039f
kelvin.param <kelvin.param@gmail.com>
2014-11-20 22:04:24 +1100

    v 0.1 of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,43 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        im <- NULL
+        
+        # if m does not exist yet, create m and initialize to NULL
+        if (!exists("m")) {
+              m <<- NULL
+        }
+        
+        #setter and getter functions
+        setmat <- function(mat) m <<- mat
+        getmat <- function() m
+        setimat <- function(imat) im <<- imat
+        getimat <- function() im
 
+
+        #make sure an argument is passed into the makeCacheMatrix function
+        if (class(x) == "matrix") {
+                #only square matrices can be inverted
+                if (nrow(x) == ncol(x)) {
+                        if (!identical(x,m)) {
+                                printvect <- cat("the matrix curently in the cache is ", getmat())
+                                print (printmat)
+                                setmat(x) #assign x to m
+                                printmat <- cat("the new matrix just stored into cache is ", getmat())
+                                print (printmat)
+                        } else {
+                                printmat <- "the new matrix is identical to stored matrix"
+                                print (printmat)
+                        }
+                } else {
+                        print ("the matrix in the argument is not square")     
+                }
+
+        } else {
+                print ("the argument is not of class matrix")
+        }
+
+        list(setmat = setmat, getmat = getmat, setimat = setimat, getimat = getimat)
 }
 
 
